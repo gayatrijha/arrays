c54419acc694371c180e4d4a72ea315e4123b589
Gayatri Jha <jha.gayatri01@gmail.com>
2024-03-31 13:18:53 +0530

    "second commmit"

--- script.js
+++ script.js
@@ -61,16 +61,180 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i, arr) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+     <div class="movements__row">
+          <div class="movements__type movements__type--${type}"> ${
+      i + 1
+    } ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const createUserName = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLocaleLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+console.log(createUserName(accounts));
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
+/////////////////////////////////////////////////
+//slice
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(1, -2));
+
+// console.log(arr.slice());
+// console.log([...arr]);
+
+// console.log(arr);
+
+// splice
+
+// console.log(arr);
+// console.log(arr.splice(2));
+// console.log(arr.splice(-1));
+// console.log(arr);
+// console.log(arr.splice(1));
+// console.log(arr);
+
+// revere
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['i', 'j', 'k', 'l'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// concat
+const letters = arr.concat(arr2);
+// console.log(letters);
+
+// console.log([...arr, ...arr2]);
+
+// join
+// console.log(letters.join(' - '));
+
+// for ..of
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const movement of movements) {
+  if (movement > 0) {
+    // console.log(`you deposited ${movement}`);
+  } else {
+    // console.log(`you withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// forEach
+
+movements.forEach(function (movement, i, array) {
+  if (movement > 0) {
+    // console.log(`movement ${i + 1}: you deposited ${movement}`);
+  } else {
+    // console.log(`movement ${i + 1} :you  withdrew ${Math.abs(movement)}`);
+  }
+});
+
+// forEach with map
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+currencies.forEach(function (value, key, map) {
+  // console.log(`${key} :${value}`);
+});
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// forEach with set
 
-/////////////////////////////////////////////////
+const currenciesUnique = new Set(['USD', 'EU', 'USD', 'GBP']);
+// console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, _v, map) {
+  // console.log(`${value} :${value}`);
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored 
+the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy.
+ A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow 
+copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or
+ a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+const j_array = [3, 5, 2, 12, 7];
+const k_array = [4, 1, 15, 8, 3];
+
+const checkDogs = function (arr1, arr2) {
+  const j_dog = arr1.slice();
+  const j_correct = j_dog.splice(1);
+  j_correct.splice(-2);
+  console.log(j_correct);
+  const totaldogs = j_correct.concat(arr2);
+  totaldogs.forEach(function (value, index) {
+    if (value >= 3) {
+      console.log(
+        `dog nummber ${index + 1} is an adult , and is ${value}  years old`
+      );
+    } else {
+      console.log(`dog nummber ${index + 1} is still a puppy`);
+    }
+  });
+};
+
+console.log(checkDogs(j_array, k_array));
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate 
+the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is
+ > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
